10c39b18ef5eae1633e404a3c8f34191b4b7e5ea
Hienphan2112 <79888195+Hienphan2112@users.noreply.github.com>
2021-04-07 09:48:49 +0700

    commit lan 1

--- LearnjapaneseFrontEnd/src/app/app.module.ts
+++ LearnjapaneseFrontEnd/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VocabularyComponent } from './Hien/vocabulary/vocabulary.component';
+import { GrammarComponent } from './Hien/grammar/grammar.component';
+import { ReviewvocabularyComponent } from './Hien/reviewvocabulary/reviewvocabulary.component';
+import { RegistrationComponent } from './Hien/registration/registration.component';
+import { LoginComponent } from './Hien/login/login.component';
+import { ReviewgrammarComponent } from './Hien/reviewgrammar/reviewgrammar.component';
+import { HistoryComponent } from './Hien/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VocabularyComponent,
+    GrammarComponent,
+    ReviewvocabularyComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ReviewgrammarComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule
