5b44a45b4d5a505c442ee122e34db7ffcf1308b2
CodeXploreRePo <hoangtuanmanh201@gmail.com>
2021-04-07 09:58:49 +0700

    Manh commit lan 1

--- LearnjapaneseFrontEnd/src/app/app.module.ts
+++ LearnjapaneseFrontEnd/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ManageruserComponent } from './manageruser/manageruser.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdduserComponent } from './adduser/adduser.component';
+import { ManageraskComponent } from './managerask/managerask.component';
+import { ListquestionComponent } from './listquestion/listquestion.component';
+import { AddquestionComponent } from './addquestion/addquestion.component';
+import { AddsucessComponent } from './addsucess/addsucess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ManageruserComponent,
+    FooterComponent,
+    AdduserComponent,
+    ManageraskComponent,
+    ListquestionComponent,
+    AddquestionComponent,
+    AddsucessComponent
   ],
   imports: [
     BrowserModule
