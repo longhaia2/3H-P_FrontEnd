a8bfb937f889f610f670bab5446a7b936f5d01a8
longhaia2 <longhaia2@users.noreply.github.com>
2021-04-15 20:56:57 +0700

    Hai commit

--- LearnjapaneseFrontEnd/src/app/Manh/sidebaradmin/sidebaradmin.component.ts
+++ LearnjapaneseFrontEnd/src/app/Manh/sidebaradmin/sidebaradmin.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {ServicebtService} from '../../Service/servicebt.service';
 
 @Component({
   selector: 'app-sidebaradmin',
   templateUrl: './sidebaradmin.component.html',
-  styleUrls: ['./sidebaradmin.component.css']
+  styleUrls: ['./sidebaradmin.component.css'],
+  providers: [ServicebtService]
 })
 export class SidebaradminComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private  lessonServiceService: ServicebtService) {
+    this.lessonServiceService.loaddCss("../../../assets/assets/css/custom.min.css");
+  }
 
   ngOnInit(): void {
   }
--- LearnjapaneseFrontEnd/src/app/Thuan/headeradmin/headeradmin.component.ts
+++ LearnjapaneseFrontEnd/src/app/Thuan/headeradmin/headeradmin.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {LessonServiceService} from '../service/lesson-service.service';
+import {ServicebtService} from '../../Service/servicebt.service';
 
 @Component({
   selector: 'app-headeradmin',
   templateUrl: './headeradmin.component.html',
-  styleUrls: ['./headeradmin.component.css']
+  styleUrls: ['./headeradmin.component.css'],
+  providers: [ServicebtService]
 })
 export class HeaderadminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  lessonServiceService: ServicebtService) {
+    this.lessonServiceService.loaddCss("../../../assets/assets/css/custom.min.css");
+  }
 
   ngOnInit(): void {
   }
