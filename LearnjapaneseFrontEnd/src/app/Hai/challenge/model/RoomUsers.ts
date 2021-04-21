4cee489bf422d7bbe461763675fe22df98731d1b
longhaia2 <63535370+longhaia2@users.noreply.github.com>
2021-04-22 03:25:22 +0700

    Delete LearnjapaneseFrontEnd directory

--- LearnjapaneseFrontEnd/src/app/Hai/challenge/model/RoomUsers.ts
+++
@@ -1,5 +0,0 @@
-export class RoomUsers {
-  user_id: number;
-  room_id: number;
-  score : number;
-}
--- LearnjapaneseFrontEnd/src/app/Hien/model/question.ts
+++
@@ -1,9 +0,0 @@
-export class Question {
-  id: number;
-  content: string;
-  ansA: string;
-  ansB: string;
-  ansC: string;
-  ansD: string;
-  ansCorrect: string;
-}
